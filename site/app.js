d69d380f3a2e2079d2e8240929288d241fe76c1d
ViniCirello <vinicius.cirello@sptech.school>
2023-05-26 12:55:39 -0300

    Backend Salas

--- site/app.js
+++ site/app.js
@@ -12,6 +12,7 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
+var salasRouter = require("./src/routes/salas") // rota para a página de salas
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", indexRouter);
+app.use("/salas", salasRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter)
--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,99 +1,70 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database fluxohealth;
 
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
+use fluxohealth;
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+create table Plano(
+	idPlano int primary key auto_increment,
+    nomePlano varchar(45),
+    descricaoPlano varchar(255),
+    valor decimal(8,2) check (valor > 0)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table Hospital(
+	idHosp int primary key auto_increment,
+    nomeHosp varchar(45),
+    cepHosp char(9),
+    numHosp char(6),
+    loginHosp varchar(60),
+    senhaHosp varchar(45),
+    cnpjHosp char(14),
+    dataContratacao date,
+    fkPlano int,
+	foreign key (fkPlano) references Plano(idPlano)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
+create table trocaDeSenhas(
+	idTroca int primary key auto_increment,
+	concluido tinyint,
+    dataHora datetime,
+    fkHosp int,
+    foreign key (fkHosp) references hospital(idHosp)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+create table Sala(
+	idSala int primary key auto_increment,
+    nomeSala varchar(45),
+    descricaoSala varchar(255),
+	fkHosp int,
+    foreign key (fkHosp) references Hospital(idHosp)
 );
 
-
-/*
-comando para sql server - banco remoto - ambiente de produção
-*/
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
+create table Poltrona(
+	idPoltrona int primary key auto_increment,
+    nomePoltrona varchar(45),
+    fkSala int,
+    foreign key (fkSala) references Sala(idSala)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+create table TipoSensor(
+	idTipo int primary key auto_increment,
+    tipo varchar(45),
+    tipoDados varchar(45)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300)
+create table Sensor(
+	idSensor int primary key auto_increment,
+    modelo varchar(45),
+    fkTipoSensor int,
+    fkPoltrona int,
+    foreign key (fkTipoSensor) references TipoSensor(idTipo),
+    foreign key (fkPoltrona) references Poltrona(idPoltrona)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
+create table Registro(
+	idRegistro int,
+    dataHora datetime,
+    valor decimal(4,2),
+    fkSensor int,
+    foreign key (fkSensor) references Sensor(idSensor),
+	primary key (idRegistro,fkSensor)
 );
-
-/*
-comandos para criar usuário em banco de dados azure, sqlserver,
-com permissão de insert + update + delete + select
-*/
-
-CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
-WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
-DEFAULT_SCHEMA = dbo;
-
-EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
-
-EXEC sys.sp_addrolemember @rolename = N'db_datareader',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
