3de43263c51b70b3e6916e04b50176b82f3e0339
FelipeCdLima <joao.batista@sptech.school>
2023-05-29 12:10:01 -0300

    Fazendo pequenas alterações no código do Dashboard

--- site/src/models/chartPoltronasModel.js
+++ site/src/models/chartPoltronasModel.js
@@ -16,23 +16,6 @@ function totalCadeiraNaSala(idSala) {
 
 function cadeirasEmUso(idSala) {
 
-    // Formatação do horário para inserir no banco de dados
-    // Está pegando em um intervalo de 5min
-    // let dataAtual = new Date()
-    // let dia = dataAtual.getDate().toString().padStart(2, '0')
-    // let mes = String(dataAtual.getMonth() + 1).padStart(2, '0')
-    // let ano = dataAtual.getFullYear()
-
-    // let hora = dataAtual.getHours()
-    // let minutoAtual = String(dataAtual.getMinutes()).padStart(2, '0')
-    // let minutoAntes = String(dataAtual.getMinutes() - 5).padStart(2, '0')
-    // let miles = dataAtual.getMilliseconds()
-    // miles = Math.floor(miles / 10);
-
-    // let dataFormatada = `${ano}-${mes}-${dia} ${hora}:${minutoAtual}:${miles}`
-    // let dataPassada = `${ano}-${mes}-${dia} ${hora}:${minutoAntes}:${miles}`
-
-
     instrucaoSql =
     `select count(distinct(fkSensor)) as qtde from Registro
         join Sensor on fkSensor = idSensor
@@ -49,7 +32,6 @@ function cadeirasEmUso(idSala) {
 
 
 function lotacaoDiariaSala(idSala){
-
     
     let data = new Date()
     let anoAtual = data.getFullYear()
@@ -57,21 +39,6 @@ function lotacaoDiariaSala(idSala){
 
     let diaAtual = String(data.getDate()).padStart(2, '0')
     let ultimoDia = String(data.getDate() - 6).padStart(2, '0')
-
-    /*
-    instrucaoSql =
-    `select date_format(dataHora, '%d/%m') as dataDiaria, count(dataHora) as contagem from Registro
-	    join Sensor on fkSensor = idSensor
-        join Sala on fkSala = idSala
-            where year(dataHora) = 2023
-              and month(dataHora) = 04
-              and day(dataHora) between 04 and 17 
-              and idSala = ${idSala}
-		    group by dataDiaria
-            order by dataDiaria asc limit 6;
-    `;
-    */
-
     
     instrucaoSql =
     `select date_format(dataHora, '%d/%m') as dataDiaria, count(dataHora) as contagem from Registro
@@ -93,41 +60,10 @@ function lotacaoDiariaSala(idSala){
 }
 
 
-
 function buscarCadeiras(idSala){
 
-    // let dataAtual = new Date()
-    // let dia = dataAtual.getDate().toString().padStart(2, '0')
-    // let mes = String(dataAtual.getMonth() + 1).padStart(2, '0')
-    // let ano = dataAtual.getFullYear()
-
-    // let hora = dataAtual.getHours()
-    // let minutoAtual = String(dataAtual.getMinutes()).padStart(2, '0')
-    // let minutoAntes = String(dataAtual.getMinutes() - 5).padStart(2, '0')
-    // let miles = dataAtual.getMilliseconds()
-    // miles = Math.floor(miles / 10);
-
-    // let dataFormatada = `${ano}-${mes}-${dia} ${hora}:${minutoAtual}:${miles}`
-    // let dataPassada = `${ano}-${mes}-${dia} ${hora}:${minutoAntes}:${miles}`
-
-
-    // instrucaoSql = 
-    // /*
-    // `select Poltrona.idPoltrona, Poltrona.nome ,Registro.valor from Poltrona 
-	// join Sensor on fkPoltrona = idPoltrona
-    // join Registro on fkSensor = idSensor
-	// join Sala on Poltrona.fkSala = Sala.idSala
-    // join Hospital on fkHospital = idHospital
-	// where Poltrona.fkSala = 1 
-	// 	  and idHospital = 1
-	// 	  and dataHora between '2023-05-18 21:45:00' and '2023-05-18 21:50:00'
-    //       order by idPoltrona;`*/
-
-    // console.log(dataPassada + "    " + dataFormatada)
-
-
     instrucaoSql =
-    `select pol.idPoltrona, pol.nomePoltrona, reg.valor from Poltrona pol
+    `select distinct(pol.idPoltrona), pol.nomePoltrona, reg.valor from Poltrona pol
 	    join Sala on fkSala = idSala 
 	    join Sensor on fkPoltrona = idPoltrona
         join TipoSensor on Sensor.fkTipoSensor = idTipo
@@ -138,76 +74,11 @@ function buscarCadeiras(idSala){
 
     return database.executar(instrucaoSql)
 
-
 }
-
-/*
-function getLotacaoSala(idSala) {
-
-    var dadosSala = {
-        qtdeVazia: 0,
-        qtdeTotal: 0
-    }
-
-
-    if (process.env.AMBIENTE_PROCESSO == "producao") {
-
-        dadosSala.qtdeVazia = cadeirasVazias(idSala)
-        dadosSala.qtdeTotal = totalCadeiraNaSala(idSala)
-
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-
-        dadosSala.qtdeVazia = cadeirasVazias(idSala)
-        dadosSala.qtdeTotal = totalCadeiraNaSala(idSala)
-
-    } else {
-
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-        return
-
-    }
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return 
-
-}
-
-function buscarMedidasEmTempoReal(idAquario) {
-
-    instrucaoSql = ''
-
-    if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
-
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
-    } else {
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-        return
-    }
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-*/
 
 module.exports = {
     totalCadeiraNaSala,
     cadeirasEmUso,
     lotacaoDiariaSala,
-    buscarCadeiras
-    //getLotacaoSala
-    //buscarMedidasEmTempoReal  
+    buscarCadeiras 
 }
--- site/src/models/salasModel.js
+++ site/src/models/salasModel.js
@@ -36,13 +36,13 @@ function InfoSalas(idHosp) {
         SELECT count(*) as TotalPoltronasUso from Registro 
         join Sensor on fkSensor = idSensor
         join TipoSensor on fkTipoSensor = idTipo
-        join poltrona on fkPoltrona = idPoltrona
+        join Poltrona on fkPoltrona = idPoltrona
         where DATE(dataHora) = DATE(now()) and 
         time(now()-500) < time(dataHora) and fkSala = S.idSala
         and idTipo = 2
     ) as totalPoltonasUso,
     (
-        SELECT count(*) as TotalPoltronas FROM poltrona 
+        SELECT count(*) as TotalPoltronas FROM Poltrona 
         JOIN Sala as SalasPoltrona on SalasPoltrona.idSala = fkSala
         WHERE fkSala = S.idSala GROUP BY fkSala
     )  as TotalPoltronas
